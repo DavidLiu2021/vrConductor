be68ba6e55089691f3771d68baaf7e50ac3d4f20
Dawei <82586287+DavidLiu2021@users.noreply.github.com>
2024-11-18 17:24:59 -0800

    Yposition detection

--- Assets/Scripts/HandVolumeController.cs
+++ Assets/Scripts/HandVolumeController.cs
@@ -7,29 +7,33 @@ public class HandVolumeController : MonoBehaviour
 {
     [Header("Audio Settings")]
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private float minVolume = 0f;
-    [SerializeField] private float maxVolume = 1f;
+    // [SerializeField] private float minVolume = 0f;
+    // [SerializeField] private float maxVolume = 1f;
 
     [Header("Hand Settings")]
-    [SerializeField] private float minYPosition = 0f;
-    [SerializeField] private float maxYPosition = 1f;
+    // [SerializeField] private Trasnform LeftHand;
+    [SerializeField] private Transform minPosition;
+    [SerializeField] private Transform maxPosition;
 
     private bool isHandInside = false;
     private Transform handTransform;
-    private float initialHandPosition;
+    // private float initialHandPosition;
+    // private float minYPosition = 0f;
+    // private float maxYPosition = 1f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        // audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other){
         if (other.CompareTag("VRhand")){
             isHandInside = true;
             handTransform = other.transform;
-            initialHandPosition = handTransform.position.y;
+            // initialHandPosition = other.transform.position.y;
+            Debug.Log("Hand inside");
         }
     }
 
@@ -43,15 +47,18 @@ public class HandVolumeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isHandInside){
-            float normalizedYPosition = Mathf.InverseLerp(minYPosition, maxYPosition, handTransform.position.y);
-            float newVolume = Mathf.Lerp(minVolume, maxVolume, normalizedYPosition);
+        if (isHandInside && handTransform != null){
+            Debug.Log("Hand inside");
+            float handY = handTransform.position.y;
+            float normalizedYPosition = Mathf.InverseLerp(minPosition.position.y, maxPosition.position.y, handY);
+
+            float newVolume = Mathf.Lerp(0f, 1f, normalizedYPosition);
             audioSource.volume = newVolume;
         }
     }
 
-    private void OnDrawGizmos(){
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(handTransform.position, transform.localScale);
-    }
+    // private void OnDrawGizmos(){
+    //     Gizmos.color = Color.yellow;
+    //     Gizmos.DrawWireCube(handTransform.position, transform.localScale);
+    // }
 }
