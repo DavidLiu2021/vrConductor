f0799652125e051ab22e0e1a35b3ef54990e0ea0
Dawei <82586287+DavidLiu2021@users.noreply.github.com>
2024-11-19 15:31:22 -0800

    two hand position detection

--- Assets/Scripts/HandVolumeController.cs
+++ Assets/Scripts/HandVolumeController.cs
@@ -7,19 +7,16 @@ public class HandVolumeController : MonoBehaviour
 {
     [Header("Audio Settings")]
     [SerializeField] private AudioSource audioSource;
-    // [SerializeField] private float minVolume = 0f;
-    // [SerializeField] private float maxVolume = 1f;
 
     [Header("Hand Settings")]
-    // [SerializeField] private Trasnform LeftHand;
     [SerializeField] private Transform minPosition;
     [SerializeField] private Transform maxPosition;
 
     private bool isHandInside = false;
     private Transform handTransform;
-    // private float initialHandPosition;
-    // private float minYPosition = 0f;
-    // private float maxYPosition = 1f;
+    private float initialHandPosition;
+    private float currentVolume;
+
 
 
     // Start is called before the first frame update
@@ -31,9 +28,10 @@ public class HandVolumeController : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         if (other.CompareTag("VRhand")){
             isHandInside = true;
+
             handTransform = other.transform;
-            // initialHandPosition = other.transform.position.y;
-            Debug.Log("Hand inside");
+            initialHandPosition = handTransform.position.y;
+            currentVolume = audioSource.volume;
         }
     }
 
@@ -48,11 +46,18 @@ public class HandVolumeController : MonoBehaviour
     void Update()
     {
         if (isHandInside && handTransform != null){
-            Debug.Log("Hand inside");
+            // Debug.Log("Hand inside");
             float handY = handTransform.position.y;
             float normalizedYPosition = Mathf.InverseLerp(minPosition.position.y, maxPosition.position.y, handY);
 
-            float newVolume = Mathf.Lerp(0f, 1f, normalizedYPosition);
+            float newVolume;
+            if (handY - initialHandPosition < 0){
+                newVolume = Mathf.Lerp(currentVolume, 0f, normalizedYPosition);
+            }else{
+                newVolume = Mathf.Lerp(currentVolume, 1f, normalizedYPosition);
+            }
+            // float newVolume = Mathf.Lerp(0f, 1f, normalizedYPosition);
+            newVolume = Mathf.Clamp01(newVolume);
             audioSource.volume = newVolume;
         }
     }
